243ced5322136e9a2147b73d44c7be4f0a14cd4c
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-03-14 16:30:33 -0500

    Add task en front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Axios from "axios";
 import Typography from '@material-ui/core/Typography';
 import { Tasks } from './components/Tasks';
+import Swal from 'sweetalert2'
+import { AddTask } from './components/AddTask';
 
 function App() {
 
   const [tasks, setTasks] = useState([]);
+
+  /*
+    const task = {
+      fechaInicio: dia + "T" + hora + ":" + minuto.toString() + ":00",
+      fechaFin: finalDate,
+      articulo: articulo
+    }*/
+
+
 
   useEffect(() => {
     Axios.get("https://ieti-lab7-frasica.azurewebsites.net/api/list-tasks?code=vGZmSjQkEVsAuw1snzPr1GAQiHaiKI73YS4svpsCiusvrhTFJO6aag==")
@@ -13,18 +24,34 @@ function App() {
         let result = response.data.response;
         setTasks(result);
       }).catch(error => {
-        alert("Error al cargar los usuarios");
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Error loading tasks. Be sure you are on Firefox and have the CorsE extension'
+        })
       });
   }, []);
 
+
+
   return (
     <div>
-      <br />
-      <Typography variant="h2" style={{ textAlign: "center" }}>Tasks:</Typography>
-      <br></br>
-      <div>
-        <Tasks list={tasks}></Tasks>
-      </div>
+
+      <section>
+        <br></br>
+        <Typography variant="h2" style={{ textAlign: "center" }}>Tasks:</Typography>
+        <br></br>
+        <div>
+          <Tasks list={tasks}></Tasks>
+        </div>
+      </section>
+
+      <section>
+        <br></br>
+        <Typography variant="h2" style={{ textAlign: "center" }}>Add task:</Typography>
+        <AddTask></AddTask>
+      </section>
+
     </div>
   );
 }
---
+++ front-end/src/components/AddTask.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import axios from "axios";
+import Swal from 'sweetalert2';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export const AddTask = (props) => {
+    const [descriptionState, setDescriptionState] = useState("");
+    const [responsibleState, setResponsibleState] = useState("");
+    const [emailState, setEmailState] = useState("");
+    const [statusState, setStatusState] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+
+    const handleDescriptionChange = (e) => {
+        setDescriptionState(e.target.value);
+    }
+
+    const handleResponsibleChange = (e) => {
+        setResponsibleState(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmailState(e.target.value);
+    }
+
+
+    const handleStatusChange = (e) => {
+        setStatusState(e.target.value);
+    }
+
+    const handleDateChange = (e) => {
+        setStartDate(e);
+    }
+
+    let [task, setTask] = useState([]);
+
+    task = {
+        description: descriptionState,
+        responsible: {
+            name: responsibleState,
+            email: emailState
+        },
+        status: statusState,
+        dueDate: startDate.toLocaleDateString()
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(task)
+        axios.post("https://ieti-lab7-frasica.azurewebsites.net/api/add-task-v2?code=3z8nk09VlhVNrLdTAVcesT9sbl1uVtFFZGjBXAUUaXg7p/o5K6GF4Q==", task)
+            .then(response => {
+                return response.data;
+            })
+            .then(Response => {
+                Swal.fire(
+                    '¡Nice!',
+                    'Task added',
+                    'success'
+                )
+            }).catch(error => {
+
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Error adding task at making the POST. Be sure you are on Firefox and have the CorsE extension'
+                })
+            });
+    }
+
+
+
+    return (
+        <div>
+            <CssBaseline />
+            <Container maxWidth="sm" align="center">
+                <Typography component="div" />
+
+                <form className="form" onSubmit={handleSubmit}>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="description">Description</InputLabel>
+                        <Input
+                            onChange={handleDescriptionChange}
+                            id="description"
+                            name="description"
+                            autoComplete="description"
+                            autoFocus
+                            value={descriptionState}
+                        />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="status">Status</InputLabel>
+                        <Select
+                            labelId="status"
+                            id="status"
+                            value={statusState}
+                            onChange={handleStatusChange}
+                        >
+                            <MenuItem value={"Ready"}>Ready</MenuItem>
+                            <MenuItem value={"In progress"}>In progress</MenuItem>
+                            <MenuItem value={"Done"}>Done</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <br/><br/><br/><br/>
+                    <Typography align="left" variant="h6" component="p" gutterBottom> Responsible: </Typography>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="responsible">Name</InputLabel>
+                        <Input
+                            onChange={handleResponsibleChange}
+                            id="responsible"
+                            name="responsible"
+                            autoComplete="responsible"
+                            autoFocus
+                            value={responsibleState}
+                        />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="responsible">E-mail</InputLabel>
+                        <Input
+                            onChange={handleEmailChange}
+                            id="responsible"
+                            name="responsible"
+                            autoComplete="responsible"
+                            autoFocus
+                            value={emailState}
+                        />
+                    </FormControl>
+
+
+                    <br/><br/><br/><br/>
+                    <Typography align="left" variant="h6" component="p" gutterBottom> Due date: </Typography>
+                    <DatePicker selected={startDate} onChange={handleDateChange} />
+ 
+
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <Button style={{ width: 300, height: 50 }} type="submit" variant="contained" color="primary">
+                        <Typography > Add task</Typography>
+                    </Button>
+
+                </form>
+            </Container>
+        </div>
+    );
+};
